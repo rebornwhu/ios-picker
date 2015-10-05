09cfa96c15ba73b150ed760a7f0806b6743b6c5d
rebornwhu <rebornwhu@gmail.com>
2015-10-04 18:44:22 -0700

    First commit

---
+++ ios-picker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-picker
+//
+//  Created by Xiao Lu on 10/4/15.
+//  Copyright © 2015 Xiao Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios-pickerTests/ios_pickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_pickerTests.swift
+//  ios-pickerTests
+//
+//  Created by Xiao Lu on 10/4/15.
+//  Copyright © 2015 Xiao Lu. All rights reserved.
+//
+
+import XCTest
+@testable import ios_picker
+
+class ios_pickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
