4017c300aefb03a659f0420f6e9790278061cfc3
rebornwhu <rebornwhu@gmail.com>
2015-10-04 21:15:08 -0700

    Finish single picker

--- ios-picker/SingleComponentPickerViewController.swift
+++ ios-picker/SingleComponentPickerViewController.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
-class SingleComponentPickerViewController: UIViewController {
+class SingleComponentPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var singlePicker: UIPickerView!
+    private let characterNames = ["Luke", "Leia", "Han", "ChewBacca", "Artoo", "Threepio", "Lando"]
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        let row = singlePicker.selectedRowInComponent(0)
+        let selected = characterNames[row]
+        let title = "You selected \(selected)"
+        
+        let alert = UIAlertController(title: title, message: "Thank you for choosing", preferredStyle: .Alert)
+        let action = UIAlertAction(title: "You're welcome", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,5 +45,20 @@ class SingleComponentPickerViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK:-
+    // MARK: Picker Data Source Methods
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return characterNames.count
+    }
+    
+    // MARK: Picker Delegate Methods
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return characterNames[row]
+    }
 
 }
