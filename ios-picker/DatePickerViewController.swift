5b1771f43a38e487cbb23f66041d114cfba8b438
rebornwhu <rebornwhu@gmail.com>
2015-10-04 19:37:21 -0700

    Get date picker working

--- ios-picker/DatePickerViewController.swift
+++ ios-picker/DatePickerViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class DatePickerViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        let date = datePicker.date
+        let message = "The date and time you selected is \(date)"
+        let alert = UIAlertController(title: "Date and Time Selected", message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "That's so true!", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let date = NSDate()
+        datePicker.setDate(date, animated: false)
         // Do any additional setup after loading the view.
     }
 
